e7f5e444f8352b1df3bcd45fec7675a24ff3a41b
mccallm <mccallm@gmail.com>
2014-08-30 10:38:09 -0400

    Added new quality method man page

--- R/qPCRset-quality-methods.R
+++ R/qPCRset-quality-methods.R
@@ -1,3 +1,6 @@
+setGeneric("quality", function(object) standardGeneric("quality"))
+setGeneric("quality<-", function(object, value) standardGeneric("quality<-"))
+
 setMethod("quality", signature(object="qPCRset"), definition = 
     function (object) {x <- assayDataElement(object, "quality"); rownames(x) <- featureNames(object); x}
 )
