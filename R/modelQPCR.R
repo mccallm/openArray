9a899d4a3a8a425ddeaab934f5eca4678bfe6de3
alexbaras <baras@jhmi.edu>
2014-08-30 17:28:18 -0400

    Update modelQPCR.R

--- R/modelQPCR.R
+++ R/modelQPCR.R
@@ -1,23 +1,25 @@
-modelQPCR <- function(Data,Cstr,isPlot=FALSE) {
-  # Data is a list with $x as the independent var, $y as the dependent var
-  # $SST is sum of squares total (y), $init are the initial paramter estimates  
+modelQPCR <- function(splitFrame,Cstr,isPlot=FALSE) {
+  # splitFrame is a data.frame from split of full table into unique cycle vs value series
   
-  localVars = list(n=length(Data$y),error=NULL,optimResults=NULL,modelType='5-parameter logistic',Data=Data,convCheck=FALSE,modelFit=NULL);
-  localVars$optimResults = constrOptim(Data$init,qpcrLogistic.robust.optim,grad=NULL,control=list(trace=0,maxit=10000000,fnscale=1),ui=Cstr$U,ci=Cstr$C,Data=Data[c("x","y","SST")],isOptim=TRUE);
-  if (class(localVars$optimResults)=='list') { if ((localVars$optimResults$convergence==0)|(localVars$optimResults$convergence==10)) { localVars$convCheck = TRUE; } }
-  if (localVars$convCheck) {
-    localVars$modelFit = qpcrLogistic.robust.optim(localVars$optimResults$par,Data,FALSE);
-  } else {
-    localVars$modelFit = list('parameters'=c(NA,NA,NA,NA,NA),'Rsq'=0);
-  }    
+  modelVars = list(n=length(splitFrame$Value),error=NULL,optimResults=NULL,modelType='5-parameter logistic',convCheck=FALSE,modelFit=NULL);
+  modelVars$SST = sum((splitFrame$Value-mean(splitFrame$Value))^2);
+  modelVars$yRange = as.numeric(quantile(splitFrame$Value,c(0.1,0.9)))
+  modelVars$init = c(modelVars$yRange[1],modelVars$yRange[2]-modelVars$yRange[1],which.min(abs(mean(modelVars$yRange)-splitFrame$Value)),0.5,1)
   
-  fitModel = localVars;
+  modelVars$optimResults = constrOptim(modelVars$init,qpcrLogistic.robust.optim,grad=NULL,control=list(trace=0,maxit=10000000,fnscale=1),ui=Cstr$U,ci=Cstr$C,Data=list(x=splitFrame$Cycle,y=splitFrame$Value,SST=modelVars$SST),isOptim=TRUE);
   
+  if (class(modelVars$optimResults)=='list') {
+  	if ((modelVars$optimResults$convergence==0)|(modelVars$optimResults$convergence==10)) {
+  		modelVars$modelFit = qpcrLogistic.robust.optim(modelVars$optimResults$par,Data=list(x=splitFrame$Cycle,y=splitFrame$Value,SST=modelVars$SST),FALSE);
+  	} else {
+  		modelVars$modelFit = list('parameters'=c(NA,NA,NA,NA,NA),'Rsq'=0);
+  	}
+  }
+
   if (isPlot) {
-    plot(Data$x,Data$y,pch='o'); 
-    lines(fitModel$modelFit$xPlot,fitModel$modelFit$yFitPlot);
+    plot(splitFrame$Cycle,splitFrame$Value,pch='o'); 
+    lines(modelVars$modelFit$xPlot,modelVars$modelFit$yFitPlot);
   }
   
-  return(fitModel);  
+  return(modelVars);  
 }
-
